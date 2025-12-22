5886401b3098373e43f52a7529ba276f8c6e42ea
cox31555-code <cox31555@gmail.com>
2025-12-22 22:50:13 +0000

    Add business enquiries contact to footer
    Added a new paragraph in the footer with the business enquiries email address (info@ajsports.ch) to provide users with a direct contact option.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,6 +11,9 @@ const Footer: React.FC = () => {
         <p className="text-[10px] text-zinc-500 leading-relaxed opacity-70">
           AJ Sports merely links/embeds content uploaded to popular media hosting websites such Vimeo.com, Dailymotion.com, Youtube.com, twitch.tv, reddit.com, etc. AJSports does not host any audiovisual content itself and has no ability to modify such content. We thus cannot accept any liability for the content transmitted by others as we are not affiliated nor endorsed by the external content. All content is copyright of their respective owners.
         </p>
+        <p className="text-[10px] text-zinc-500 leading-relaxed opacity-70">
+          For business enquiries: info@ajsports.ch
+        </p>
       </div>
       <p className="text-[9px] font-black uppercase tracking-[0.2em] text-zinc-800 border-t border-white/5 pt-8 mt-8">
         © 2025 AJ Sports, Inc. All rights reserved
